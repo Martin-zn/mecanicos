2e9ef9acd304361fcbd25216b34421099e9ef4ae
Martin Zuñiga <martin.zuniga@usach.cl>
2024-10-07 11:38:53 -0300

    listo para el deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
+  output: 'server',
+  adapter: vercel(),
+  // experimental: {
+  //   viewTransitions: true,
+  // },
   integrations: [tailwind(), compress()],
 });
