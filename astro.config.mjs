3f07d20ed35510a05d405d3bb8f5fd8eed2eb986
Martin Zuñiga <martin.zuniga@usach.cl>
2024-10-09 21:03:59 -0300

    netlify implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-import vercel from '@astrojs/vercel/serverless';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+    adapter: netlify(),
   // experimental: {
   //   viewTransitions: true,
   // },
